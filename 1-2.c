f09b9df0a60489a04aaa590f9337395b80ca737b
ZaleJ <947703213@qq.com>
2016-06-15 21:13:32 +0800

    Update 1-2.c

--- 1-2.c
+++ 1-2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <conio.h>
 #define MaxSize 10
-typedef int ElemType ;  // ½«int¶¨ÒåÎªElemType
+typedef int ElemType ;  // å°†intå®šä¹‰ä¸ºElemType
 
 typedef struct{
     int *elem;
@@ -9,8 +9,8 @@ typedef struct{
     int listsize;
 } Sqlist;
 
-//³õÊ¼»¯Ë³Ðò±í
-//L£ºSqlistÀàÐÍµÄÖ¸Õë
+//åˆå§‹åŒ–é¡ºåºè¡¨
+//Lï¼šSqlistç±»åž‹çš„æŒ‡é’ˆ
 void initSqlist(Sqlist *L){
     L->elem=(int *)malloc(MaxSize*sizeof(ElemType));
     if(!L->elem) exit(0);
@@ -18,10 +18,26 @@ void initSqlist(Sqlist *L){
     L->listsize= MaxSize;
 }
 
-
+void insertElem(Sqlist *L, int i, ElemType item){
+    /*add element "item" to the list*/
+    ElemType *base, *insertPtr, *p;
+    if(i<1 || i>L->length+1) exit(0);
+    if(L->length >= L->listsize){
+        base=(ElemType*)realloc(L->elem, (L->listsize+10)*sizeof(ElemType));
+        L->elem=base;
+        L->listsize=L->listsize+100;
+    }
+    insertPtr = &(L->elem[i-1]);
+    for(p=&(L->elem[L->length-1]); p >= insertPtr; p--)
+        *(p+1) = *p;
+    *insertPtr=item;
+    L->length++;
+}
 
 main(){
     Sqlist l;
     int i;
     initSqlist(&l);
+    for(i=0; i<15; i++)
+        insertElem(&l, i+1, i+1);
 }
