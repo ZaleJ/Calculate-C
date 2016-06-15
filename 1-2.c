effa387154c55c7acadcf8ef90bf3e46e5736406
ZaleJ <947703213@qq.com>
2016-06-15 21:26:00 +0800

    Update 1-2.c

--- 1-2.c
+++ 1-2.c
@@ -18,7 +18,7 @@ void initSqlist(Sqlist *L){
     L->listsize= MaxSize;
 }
 
-void insertElem(Sqlist *L, int i, ElemType item){
+void InsertElem(Sqlist *L, int i, ElemType item){
     /*add element "item" to the list*/
     ElemType *base, *insertPtr, *p;
     if(i<1 || i>L->length+1) exit(0);
@@ -34,10 +34,28 @@ void insertElem(Sqlist *L, int i, ElemType item){
     L->length++;
 }
 
+void DelElem(Sqlist *L, int i){
+    /*Delete number i element from the list L*/
+    ElemType *delItem, *q;
+    if(i<1 || i > L->length) exit(0);
+    delItem=&(L->elem[i-1]);
+    q = L->elem+L->length-1;
+    for(++delItem; delItem<=q; ++delItem)   *(delItem-1) = *delItem;
+    L->length--;
+}
+
 main(){
     Sqlist l;
     int i;
     initSqlist(&l);
     for(i=0; i<15; i++)
-        insertElem(&l, i+1, i+1);
+        InsertElem(&l, i+1, i+1);
+    printf("\nThe content of the list is\n");
+    for(i=0; i<l.length; i++)
+        printf("%d ", l.elem[i]);
+    DelElem(&l, 5);
+    printf("\nDelete the fifth element\n");
+    for(i=0; i<l.length; i++)
+        printf("%d ", l.elem[i]);
+    getche();
 }
